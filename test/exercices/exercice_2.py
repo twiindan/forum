445bf6b6c7d7b64d31ea224bb6b17eaff345d4c0
Toni Robres <arobres@tid.es>
2015-05-12 21:36:28 +0200

    add second exercice

--- test/exercices/exercice_2.py
+++ test/exercices/exercice_2.py
@@ -1,84 +1,94 @@
-__author__ = 'arobres'
-
 import requests
-from nose.tools import assert_equals, assert_true
+from nose.tools import assert_equals
 import ujson
 
-#make a POST request
 
-response = requests.post('http://localhost:8081/v1.0/users')
+'''
+E2E SCENARIO
+CREATE A USER
+SEND A MESSAGE TO THE USER
+OBTAIN ALL USER MESSAGES (YOU NEED AUTHENTICATION)
+'''
 
+'''
+FIRST WE NEED CREATE THE USER
+URL: http://localhost:8081/v1.0/users
+ACTION: POST
+THE BODY SHOULD HAVE THE FOLLOWING MANDATORY PARAMETERS: name, username, password, role, email
+THE ROLE SHOULD BE ONE OF THE FOLLOWING OPTIONS: QA, DEVELOPER, MANAGER
+ASSERT THE STATUS CODE (200) and the response content (user created)
+'''
 
-#Assert response
+#DEFINE THE VARIABLES TO BE USED
+username = 'expoqa'
+password = 'expoqa'
+message_to_send = 'Hello user! Is your first message!'
+create_user_url = 'http://localhost:8081/v1.0/users'
+send_message_url = 'http://localhost:8081/v1.0/users/inbox/expoqa'
 
-assert_true(response.ok, 'BAD REQUEST!!!!!, Response obtained is: {} {}'.format(response.status_code, response.content))
-#We obtain a Assertion Error because server responses with 400 Bad Request
-
-
-#Inserting data in the POST request
-#We need include a JSON with all the data required
-"""{'name': <string>,
-     'username': <string>,
-     'password': <string>,
-     'role': <string>,
-     'email': <string>
-    }
-"""
-
-#How we can build JSON?
-
-#First option
-body = u"{'name': 'toni', 'username': 'arobres', 'password': 'tid', 'role': 'QA', 'email': 'arobres@tid.es'}'"
-
-print body
-print type(body)
-
-string_body = ujson.dumps(body)
-print string_body
+#DEFINE THE BODY TO CREATE A NEW USER
 
 
-#It's ok but... one remind...
-
-#JSON can be mapped directly with Dicts in Python
-body = {'name': 'toni', 'username': 'antonio_robres', 'password': 'tid', 'role': 'QA', 'email': 'arobres@tid.es'}
-
-print body
-print type(body)
-
-dict_body = ujson.dumps(body)
-print dict_body
+#CONVERT THE PYTHON DICTIONARY IN JSON WITH ujson.dumps
 
 
-#Another option
-
-new_json_body = {}
-new_json_body['name'] = 'toni'
-new_json_body['username'] = 'toni'
-new_json_body['password'] = 'tid'
-new_json_body['role'] = 'QA'
-new_json_body['email'] = 'arobres@tid.es'
-
-print new_json_body
-new_json_body = ujson.dumps(new_json_body)
-print new_json_body
-
-#OK... Now we can do the POST request
-
-response = requests.post(url='http://localhost:8081/v1.0/users', data=new_json_body)
-
-#Assert response
-
-assert_true(response.ok, 'BAD REQUEST!!!!!, Response obtained is: {} {}'.format(response.status_code, response.content))
-response_body = response.content
-
-assert_equals(response_body, 'user created', 'INCORRECT RESPONSE BODY. RESPONSE OBTAINED IS: {}'.format(response_body))
+#SEND THE POST REQUEST WITH THE BODY CREATED
 
 
-#BE CAREFUL WITH THE RESPONSE WHEN TRY TO CONVERT TO JSON:
+#ASSERT THE STATUS CODE AND RESPONSE CONTENT
+#ASSERT THE RESPONSE CONTENT
 
-response_body = response.json()
 
 
-#### LEARNING BY DOING  ####
+'''
+THE SECOND PART IS SEND A MESSAGE TO THE USER CREATED
+URL: http://localhost:8081/v1.0/users/inbox/{username}
+ACTION: POST
+THE BODY SHOULD HAVE THE FOLLOWING MANDATORY PARAMETERS: message
+ASSERT THE STATUS CODE (200) AND THE CONTENT OF THE MESSAGE (message saved)
 
-#MAKE A REQUEST TO PUBLISH A MESSAGE TO THE FORUM
+'''
+
+#DEFINE THE BODY TO SEND A MESSAGE
+
+
+#CONVERT THE PYTHON DICTIONARY IN JSON WITH ujson.dumps
+
+
+#SEND THE POST REQUEST WITH THE BODY CREATED
+
+
+#ASSERT THE STATUS CODE AND RESPONSE CONTENT
+#ASSERT THE RESPONSE CONTENT
+
+
+
+'''
+THE LAST PART IS OBTAIN ALL THE MESSAGES FROM THE USER INBOX
+URL: http://localhost:8081/v1.0/users/inbox/{username}
+ACTION: GET
+ASSERT THE STATUS CODE (200), THE NUMBER OF MESSAGES (1) AND THE CONTENT OF THE MESSAGE
+REMEBER THAT THIS REQUEST NEEDS AUTHENTICATION
+'''
+
+
+#DEFINE THE AUTHENTICATION TUPLE
+
+
+#SEND THE GET REQUEST WITH THE AUTHENTICATION
+
+
+#ASSERT THE STATUS CODE
+
+
+#CONVERT THE RESPONSE CONTENT TO DICT USING THE FUNCTION .json()
+
+
+#ASSERT THE USERNAME
+
+
+#ASSERT THE NUMBER OF MESSAGES
+
+
+#ASSERT THE MESSAGE RECEIVED
+
