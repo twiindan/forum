35c69e9254a97465eba054f3ad1dc6c44a205254
Toni Robres <arobres@tid.es>
2015-05-12 21:36:16 +0200

    add first exercice

--- test/exercices/exercice_1.py
+++ test/exercices/exercice_1.py
@@ -1,70 +1,41 @@
-__author__ = 'arobres'
-
 import requests
-from nose.tools import assert_equals, assert_true
+from nose.tools import assert_equals
+import ujson
 
 
-#Make a GET request to the forum server
+#MAKE A REQUEST TO PUBLISH A MESSAGE TO THE FORUM
+'''
+The URL is http://localhost:8081/v1.0/forum
+THE BODY SHOULD HAVE THE FOLLOWING MANDATORY PARAMETERS: theme, subject and message
+THE THEME SHOULD BE ONE OF THE FOLLOWING OPTIONS: Security, Development, Automation, Testing
+ASSERT THE STATUS CODE (200) and the response content (message created)
+MAKE A GET REQUEST TO OBTAIN ALL THE MESSAGES PUBLISHED IN THE FORUM
+'''
 
-response = requests.get('http://localhost:8081/v1.0')
+url = 'http://localhost:8081/v1.0/forum'
 
-
-#get the response status code
-
-status_code = response.status_code
-print "RESPONSE STATUS CODE: {}".format(status_code)
-
-
-#verify the response status code
-
-assert_true(response.ok, 'BAD REQUEST!!!!!!')
-assert_equals(status_code, 200, 'INCORRECT STATUS CODE. STATUS CODE OBTAINED IS: {}'.format(status_code))
+#DEFINE THE BODY IN A DICTIONARY WITH THE PARAMETERS REQUIRED
 
 
-#Save the body obtained in the response in a <string>
-
-text_body = response.text
-print "RESPONSE BODY: {}".format(text_body)
-print "RESPONSE BODY IS A: {}".format(type(text_body))
+#CONVERT THE PYTHON DICTIONARY IN JSON WITH ujson.dumps
 
 
-#Save the body obtained in the response in a <dict>
-
-json_body = response.json()
-print "JSON RESPONSE BODY: {}".format(json_body)
-print "JSON RESPONSE IS A: {}".format(type(json_body))
+#SEND THE POST REQUEST
 
 
-#Get the response body values and verify it
-
-assert_equals(json_body['product'], 'forum', 'PRODUCT IS NOT CORRECT')
-assert_equals(json_body['version'], '2.0', 'VERSION IS NOT CORRECT')
+#ASSERT THE STATUS CODE
 
 
-#Get the response headers
-
-response_header = response.headers
-print "RESPONSE HEADER: {}".format(response_header)
+#ASSERT THE RESPONSE CONTENT
 
 
-#Get the content-type response header
-
-print "CONTENT-TYPE HEADER: {}".format(response_header['content-type'])
-assert response_header['content-type'] == 'application/json'
+#SEND A REQUEST TO OBTAIN ºALL THE MESSAGES FROM THE FORUM
 
 
-#Make a POST request to the forum server without content
-
-response = requests.post('http://localhost:8081/v1.0')
-status_code = response.status_code
-print "RESPONSE STATUS CODE: {}".format(status_code)
+#ASSERT THE STATUS CODE
 
 
-#Verify the status code
-
-assert_equals(status_code, 405, 'INCORRECT STATUS CODE. STATUS CODE OBTAINED IS: {}'.format(status_code))
+#CONVERT THE RESPONSE CONTENT TO JSON
 
 
-#Verify an incorrect status code
-
-assert_equals(status_code, 200, 'INCORRECT STATUS CODE. STATUS CODE OBTAINED IS: {}'.format(status_code))
+#ASSERT THE MESSAGE
