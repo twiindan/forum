3b4948b677e9078fd12a049b6bebe5c3a13a8626
Toni Robres <arobres@tid.es>
2015-05-12 13:30:26 +0200

    add constants

--- test/commons/constants.py
+++ test/commons/constants.py
@@ -6,5 +6,14 @@ THEME = u'theme'
 SUBJECT = u'subject'
 MESSAGES = u'message'
 FORUM_KEYS = [THEME, SUBJECT, MESSAGES]
-FORUM_THEMES = ['Security', 'Development', 'Automation', 'Testing']
+FORUM_THEMES = [u'Security', u'Development', u'Automation', u'Testing']
+
+# USER BODY PARAMETERS
+
+NAME = u'name'
+USERNAME = u'username'
+PWD = u'password'
+ROLE = u'role'
+EMAIL = u'email'
+USER_ROLES = [u'QA', u'DEVELOPER', u'MANAGER']
 
